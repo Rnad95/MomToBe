acdd5bf660948239dcf2e362276a2e4a1510bf49
BaraahAbusara <baraaabusara@gmail.com>
2022-06-26 00:11:49 +0300

    settings and profile work Bara'ah and Hadeel

--- app/src/main/java/com/example/momtobe/Profile.java
+++ app/src/main/java/com/example/momtobe/Profile.java
@@ -5,72 +5,68 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.amplifyframework.api.graphql.model.ModelQuery;
+import com.amplifyframework.core.Amplify;
 import com.amplifyframework.datastore.generated.model.Mother;
 
 public class Profile extends AppCompatActivity {
+    private static final String TAG = "profile";
     Mother mother ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-//        String motherUsername = getIntent().getStringExtra(USERNAME);
+        int motherId = 0 ; // only to avoid errors
+//        motherId = getIntent().getStringExtra(ID);
+        Amplify.API.query(
+                ModelQuery.list(Mother.class),
+                success->{
+                    if(success.hasData())
+                    {
+                        for (Mother mother : success.getData())
+                        {
+                            if(mother.getId().equals(motherId)){
+                                setMotherInfo(mother);
+                                setMotherImage(mother);
+                                setRecyclerFragment(mother);
+                            }
+                        }
+                    }
+                },
+                fail->{
+                    Log.i(TAG, "onCreate: failed to find mother in database");
+                }
+        );
 
-        setMotherInfo();
-        setMotherImage();
-        setRecycler();
-        setButtons();
+
     }
 
-    void setMotherImage(){
+    void setMotherImage(Mother mother){  //TODO waiting for s3 -> hamze
         ImageView motherImage = findViewById(R.id.pro_profile_picture);
 
-
     }
 
-    void setMotherInfo(){
+    void setMotherInfo(Mother mother){
+
         TextView motherName = findViewById(R.id.pro_mother_name);
-        TextView motherEmail = findViewById(R.id.pro_mother_email);
         TextView motherPhone = findViewById(R.id.pro_mother_phone);
         TextView motherNumberOfChildren = findViewById(R.id.pro_mother_number_of_children);
 
+        motherName.setText(mother.getName());
+        motherPhone.setText(mother.getPhoneNumber());
+        motherNumberOfChildren.setText(mother.getNumOfChildren());
 
+    }
 
-
+    void setRecyclerFragment(Mother mother){
 
     }
 
-    void setRecycler(){
-        RecyclerView recyclerView = findViewById(R.id.pro_list);
-
-
-    }
-    void setButtons(){
-        RecyclerView recyclerView = findViewById(R.id.pro_list);
-
-        Button myFavBlogs = findViewById(R.id.pro_fav_blogs);
-        myFavBlogs.setOnClickListener(view->{
-
-        });
-
-        Button myExperience= findViewById(R.id.pro_mother_experience);
-        myExperience.setOnClickListener(view->{
-
-        });
-
-        Button myProducts= findViewById(R.id.pro_mother_products);
-        myProducts.setOnClickListener(view->{
-
-        });
-
-        Button myQuestions = findViewById(R.id.pro_mother_questions);
-        myQuestions.setOnClickListener(view->{
-
-        });
-
-    }
 }
