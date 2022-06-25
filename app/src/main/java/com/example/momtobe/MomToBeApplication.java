51b36f0b207a1a47fcda4206314f3fadd07cf5c2
Hamzh Freijat <hamzhg2016@gmail.com>
2022-06-25 16:50:12 +0300

    New Changes

--- app/src/main/java/com/example/momtobe/MomToBeApplication.java
+++ app/src/main/java/com/example/momtobe/MomToBeApplication.java
@@ -7,6 +7,7 @@ import com.amplifyframework.AmplifyException;
 import com.amplifyframework.api.aws.AWSApiPlugin;
 import com.amplifyframework.core.Amplify;
 import com.amplifyframework.datastore.AWSDataStorePlugin;
+import com.amplifyframework.storage.s3.AWSS3StoragePlugin;
 
 public class MomToBeApplication extends Application {
 
@@ -23,6 +24,8 @@ public class MomToBeApplication extends Application {
 
         try {
             Amplify.addPlugin(new AWSDataStorePlugin());
+            Amplify.addPlugin(new AWSS3StoragePlugin());
+            Amplify.addPlugin(new AWSApiPlugin());
             Amplify.configure(getApplicationContext());
 
             Log.i(TAG, "Initialized Amplify");
